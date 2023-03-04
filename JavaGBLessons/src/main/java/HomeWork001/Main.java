d403d9f2fab85fce421f987e0a5d618e339e443e
MichaelSv <michaelsvistunoff@gmail.com>
2023-03-05 05:45:36 +0600

    Task-1 is completed

--- JavaGBLessons/src/main/java/HomeWork001/Main.java
+++ JavaGBLessons/src/main/java/HomeWork001/Main.java
@@ -1,5 +1,7 @@
 package HomeWork001;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -15,6 +17,29 @@ public class Main {
             Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного,
             а остальные - равны ему.
         */
+        findMinAndMaxElementsOfArray(generateRandomArray());
+    }
 
+    private static int[] generateRandomArray() {
+        int[] newArray = new int[10];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = (int)Math.round((Math.random() * 30) - 20 );
+        }
+        System.out.println("Our random array is:");
+        System.out.println(Arrays.toString(newArray));
+        return newArray;
+    }
+
+    private static void findMinAndMaxElementsOfArray(int[] array) {
+        int numMax = array[0];
+        int numMin = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > numMax) {
+                numMax = array[i];
+            } else if (array[i] < numMin) {
+                numMin = array[i];
+            }
+        }
+        System.out.printf("\nMinial number is %d. Maximal number is %d", numMin, numMax);
     }
 }
