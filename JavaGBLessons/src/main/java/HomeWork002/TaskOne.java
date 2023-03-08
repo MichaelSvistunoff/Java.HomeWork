6c53070f55abaa3da11d56c5012d281da86fd6f9
MichaelSv <michaelsvistunoff@gmail.com>
2023-03-08 11:57:16 +0600

    Task 1 is completed

--- JavaGBLessons/src/main/java/HomeWork002/TaskOne.java
+++ JavaGBLessons/src/main/java/HomeWork002/TaskOne.java
@@ -8,5 +8,22 @@ public class TaskOne {
         Напишите метод, который принимает на вход строку (String)
         и определяет является ли строка палиндромом (возвращает boolean значение).
         */
+        System.out.println(isPolindrome());
+    }
+
+    private static boolean isPolindrome() {
+        // Create a new Scanner and String from user's input
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word to check on polindrome: ");
+        String wordToCheck = sc.next();
+        // Make array from string
+        char[] newArr = wordToCheck.toCharArray();
+        // Make checker
+        boolean isTrue = false;
+
+        for (int i = 0; i < newArr.length / 2; i++) {
+            isTrue = newArr[i] == newArr[newArr.length - 1 - i];
+        }
+        return isTrue;
     }
 }
