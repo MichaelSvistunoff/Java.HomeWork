100751197e0d33c7c80eb3e1420439d744dca69a
MichaelSv <michaelsvistunoff@gmail.com>
2023-03-08 11:54:47 +0600

    Seminar 2 initial commit

---
+++ JavaGBLessons/src/main/java/HomeWork002/TaskOne.java
@@ -0,0 +1,12 @@
+package HomeWork002;
+
+import java.util.Scanner;
+
+public class TaskOne {
+    public static void main(String[] args) {
+        /*
+        Напишите метод, который принимает на вход строку (String)
+        и определяет является ли строка палиндромом (возвращает boolean значение).
+        */
+    }
+}
