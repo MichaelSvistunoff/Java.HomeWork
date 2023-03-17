500d3c9767658cf9cf668550f41782cb39d4ebbf
MichaelSv <michaelsvistunoff@gmail.com>
2023-03-18 01:56:51 +0600

    HomeWork4 initial commit

---
+++ JavaGBLessons/src/main/java/HomeWork004/TaskTwo.java
@@ -0,0 +1,10 @@
+package HomeWork004;
+
+public class TaskTwo {
+    public static void main(String[] args) {
+        /*
+        Пусть дан LinkedList с несколькими элементами.
+        Реализуйте метод, который вернет “перевернутый” список.
+        */
+    }
+}
